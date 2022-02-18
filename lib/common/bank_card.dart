6a7047de82aac9262eda18c4434914c18e4c965d
adolesamuel <adolesamuel4@gmail.com>
2022-02-18 13:23:23 +0100

    update bank card

--- lib/common/bank_card.dart
+++ lib/common/bank_card.dart
@@ -3,31 +3,65 @@ import 'package:flutter/material.dart';
 class BankCardWidget extends StatelessWidget {
   const BankCardWidget({Key? key}) : super(key: key);
 
+  final bool verified = true;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          color: Colors.blue, borderRadius: BorderRadius.circular(10.0)),
-      child: Column(children: [
-        //Card Name and Expiry date
-        Row(
-          children: const [
-            //Should be image
-            Text('VISA'),
-            Text('08/28'),
-          ],
-        ),
-        //Cash Symbol and Card Cash Value
-        Row(
-          children: const [
-            Text('\$'),
-            Text('1,260.28'),
-          ],
-        ),
+    return Center(
+      child: Container(
+        height: 150.0,
+        width: 300.0,
+        margin: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        decoration: BoxDecoration(
+            color: Colors.blue, borderRadius: BorderRadius.circular(10.0)),
+        child: Column(
+          children: [
+            //Card Name and Expiry date
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    const Image(
+                      image: AssetImage('assets/visa-2.png'),
+                      width: 50,
+                      height: 50,
+                    ),
+                    if (verified) ...[
+                      const SizedBox(
+                        width: 8.0,
+                      ),
+                      const CircleAvatar(
+                        radius: 8,
+                        backgroundColor: Color.fromARGB(255, 152, 143, 221),
+                        child: Center(
+                          child: Icon(
+                            Icons.check,
+                            size: 10,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ],
+                ),
+                Text('08/28'),
+              ],
+            ),
+            //Cash Symbol and Card Cash Value
+            Row(
+              children: const [
+                Text('\$'),
+                Text('1,260.28'),
+              ],
+            ),
 
-        //Card Number
-        const Text('**** **** **** 7735'),
-      ]),
+            //Card Number
+            const Text('**** **** **** 7735'),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sendme_work/common/bank_card.dart';
 import 'package:sendme_work/common/payment_method_widget.dart';
 import 'package:sendme_work/common/top_bar.dart';
 
@@ -17,41 +18,71 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.white,
         elevation: 0,
       ),
-      body: Column(
-        children: [
-          TopBar(),
-          const Divider(
-            color: Colors.grey,
-            height: 1.0,
-          ),
-          //Card
-          SizedBox(
-            height: 200,
-            child: ListView(
-              scrollDirection: Axis.horizontal,
-              children: const [
-                PaymentMethodWidget(
-                  paymentMethodName: 'PayPal',
-                  paymentMethodImage: AssetImage('assets/paypal.png'),
-                ),
-                PaymentMethodWidget(
-                  paymentMethodName: 'Amazon Pay',
-                  paymentMethodImage: AssetImage('assets/amazon.png'),
-                ),
-                PaymentMethodWidget(
-                  paymentMethodName: 'Amazon Pay',
-                  paymentMethodImage: AssetImage('assets/amazon.png'),
-                ),
-                PaymentMethodWidget(
-                  paymentMethodName: 'Amazon Pay',
-                  paymentMethodImage: AssetImage('assets/amazon.png'),
-                ),
-              ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Column(
+          children: [
+            TopBar(),
+            const Divider(
+              color: Colors.grey,
+              height: 1.0,
             ),
-          ),
+            //Card
+            SizedBox(
+              height: 200,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: const [
+                  PaymentMethodWidget(
+                    paymentMethodName: 'PayPal',
+                    paymentMethodImage: AssetImage('assets/paypal.png'),
+                  ),
+                  PaymentMethodWidget(
+                    paymentMethodName: 'Amazon Pay',
+                    paymentMethodImage: AssetImage('assets/amazon.png'),
+                  ),
+                  PaymentMethodWidget(
+                    paymentMethodName: 'Amazon Pay',
+                    paymentMethodImage: AssetImage('assets/amazon.png'),
+                  ),
+                  PaymentMethodWidget(
+                    paymentMethodName: 'Amazon Pay',
+                    paymentMethodImage: AssetImage('assets/amazon.png'),
+                  ),
+                ],
+              ),
+            ),
+            //
+            InkWell(
+              onTap: () {},
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Text(
+                    'Super Card',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  Icon(Icons.chevron_right),
+                ],
+              ),
+            ),
 
-          //Super Cardkk
-        ],
+            const SizedBox(
+              height: 8.0,
+            ),
+
+            //Super Card
+            SizedBox(
+              height: 200,
+              child:
+                  ListView(scrollDirection: Axis.horizontal, children: const [
+                BankCardWidget(),
+                BankCardWidget(),
+                BankCardWidget(),
+              ]),
+            )
+          ],
+        ),
       ),
     );
   }
