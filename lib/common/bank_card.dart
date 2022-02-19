cb9539523a903469d880f455295bf9386f56d499
adolesamuel <adolesamuel4@gmail.com>
2022-02-19 19:49:25 +0100

    Gave card sweet semi circle

--- lib/common/bank_card.dart
+++ lib/common/bank_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:number_display/number_display.dart';
 import 'package:sendme_work/common/card_data.dart';
+import 'package:sendme_work/common/half_semi_circle.dart';
 import 'package:sendme_work/core/utils/color.dart';
 
 class BankCardWidget extends StatelessWidget {
@@ -32,88 +33,113 @@ class BankCardWidget extends StatelessWidget {
         height: 150.0,
         width: 350.0,
         margin: const EdgeInsets.all(8.0),
-        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
         decoration: BoxDecoration(
             color: cardData.cardColor,
             borderRadius: BorderRadius.circular(10.0)),
-        child: Column(
+        child: Stack(
           children: [
-            //Card Name and Expiry date
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    Image(
-                      image: AssetImage(cardImage(cardData.cardCompany)!),
-                      width: 50,
-                      height: 50,
-                      color: cardData.cardCompany != CardCompany.visa
-                          ? null
-                          : cardData.cardColor.computeLuminance() > 0.5
-                              ? Colors.black
-                              : Colors.white,
-                    ),
-                    if (cardData.verified) ...[
-                      const SizedBox(
-                        width: 8.0,
-                      ),
-                      const CircleAvatar(
-                        radius: 8,
-                        backgroundColor: Color.fromARGB(255, 152, 143, 221),
-                        child: Center(
-                          child: Icon(
-                            Icons.check,
-                            size: 10,
-                            color: Colors.black,
+            //Background artifact
+            Align(
+              alignment: Alignment.centerRight,
+              child: Container(
+                height: 150,
+                width: 150,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                clipBehavior: Clip.hardEdge,
+                child: HalfSemiCircle(
+                  color: cardData.cardColor,
+                ),
+              ),
+            ),
+
+            //Card Contents
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+              child: Column(
+                children: [
+                  //Card Name and Expiry date
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Image(
+                            image: AssetImage(cardImage(cardData.cardCompany)!),
+                            width: 50,
+                            height: 50,
+                            color: cardData.cardCompany != CardCompany.visa
+                                ? null
+                                : cardData.cardColor.computeLuminance() > 0.5
+                                    ? Colors.black
+                                    : Colors.white,
                           ),
-                        ),
+                          if (cardData.verified) ...[
+                            const SizedBox(
+                              width: 8.0,
+                            ),
+                            const CircleAvatar(
+                              radius: 8,
+                              backgroundColor:
+                                  Color.fromARGB(255, 152, 143, 221),
+                              child: Center(
+                                child: Icon(
+                                  Icons.check,
+                                  size: 10,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ],
+                      ),
+                      Text(
+                        cardData.expiryDate,
+                        style: TextStyle(color: textColor),
                       ),
                     ],
-                  ],
-                ),
-                Text(
-                  cardData.expiryDate,
-                  style: TextStyle(color: textColor),
-                ),
-              ],
-            ),
-            //Cash Symbol and Card Cash Value
-            SizedBox(
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    '\$',
-                    style: TextStyle(color: textColor),
+                  ),
+                  //Cash Symbol and Card Cash Value
+                  SizedBox(
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '\$',
+                          style: TextStyle(color: textColor),
+                        ),
+                        const SizedBox(
+                          width: 4.0,
+                        ),
+                        Text(
+                          displayValue(cardData.cardValue),
+                          style: TextStyle(
+                            color: textColor,
+                            fontSize: 24.0,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                   const SizedBox(
-                    width: 4.0,
+                    height: 16.0,
                   ),
+
+                  //Card Number
                   Text(
-                    displayValue(cardData.cardValue),
+                    '**** **** **** ${cardData.last4Number}',
+                    textAlign: TextAlign.center,
                     style: TextStyle(
-                      color: textColor,
-                      fontSize: 24.0,
-                    ),
+                        fontWeight: FontWeight.bold,
+                        color: textColor,
+                        fontSize: 18,
+                        wordSpacing: 25,
+                        letterSpacing: 4.0),
                   ),
                 ],
               ),
-            ),
-            const SizedBox(
-              height: 16.0,
-            ),
-
-            //Card Number
-            Text(
-              '**** **** **** ${cardData.last4Number}',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: textColor,
-                  fontSize: 18,
-                  wordSpacing: 25,
-                  letterSpacing: 4.0),
             ),
           ],
         ),
---
+++ lib/common/half_semi_circle.dart
@@ -0,0 +1,93 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class HalfSemiCircle extends StatelessWidget {
+  final double diameter;
+  final Color color;
+
+  const HalfSemiCircle({
+    Key? key,
+    this.diameter = 200,
+    required this.color,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      painter: MyPainter(color),
+      size: Size(diameter, diameter),
+    );
+  }
+}
+
+// This is the Painter class
+class MyPainter extends CustomPainter {
+  final Color color;
+
+  MyPainter(this.color);
+
+  Color lighten(Color c, [int percent = 10]) {
+    assert(1 <= percent && percent <= 100);
+    var p = percent / 100;
+    return Color.fromARGB(
+      c.alpha,
+      c.red + ((255 - c.red) * p).round(),
+      c.green + ((255 - c.green) * p).round(),
+      c.blue + ((255 - c.blue) * p).round(),
+    );
+  }
+
+  Color darken(Color c, [int percent = 10]) {
+    assert(1 <= percent && percent <= 100);
+    var f = 1 - percent / 100;
+    return Color.fromARGB(
+      c.alpha,
+      (c.red * f).round(),
+      (c.green * f).round(),
+      (c.blue * f).round(),
+    );
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()
+      ..shader = LinearGradient(colors: [
+        color,
+        color.computeLuminance() > 0.5 ? darken(color, 10) : lighten(color, 10)
+      ]).createShader(Rect.fromCenter(
+        center: Offset(size.height / 2, size.width / 2),
+        height: size.height,
+        width: size.width,
+      ));
+
+    //draw one semi circle
+    canvas.drawArc(
+      Rect.fromCenter(
+        center: Offset(size.height, size.width / 2),
+        height: size.height,
+        width: size.width,
+      ),
+      pi,
+      pi / 2,
+      true,
+      paint,
+    );
+
+    //draw other semi circle
+    canvas.drawArc(
+      Rect.fromCenter(
+        center: Offset(0, size.width / 2),
+        height: size.height,
+        width: size.width,
+      ),
+      0,
+      pi / 2,
+      true,
+      paint,
+    );
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
+}
